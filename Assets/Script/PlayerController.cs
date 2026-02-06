43e157f963c7ff1ec499efdc3441d6333e268b89
Wonseunghu <minuszz@naver.com>
2026-02-06 12:51:56 +0900

    2026.2.6 카메라세팅, 사격, 점프 사격점프 움직임사격 전부 구현, 완료

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -7,9 +7,13 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 7f;
     public float jumpForce = 14f;
 
+    [Header("공격 설정")]
+    public float shootDelay = 0.3f; // 공격 간격 (0.2초 정도가 적당)
+    private float lastShootTime;
+
     [Header("땅체크 설정")]
     public Transform groundCheck; // �߹� ��ġ (�� ������Ʈ)
-    public float groundCheckRadius = 0.2f;
+    public float groundCheckRadius = 0.1f;
     public LayerMask groundLayer; // Ground ���̾� ���� �ʿ�
 
     private Rigidbody2D rb;
@@ -47,17 +51,27 @@ public class PlayerController : MonoBehaviour
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
-
-        // 5. 공격 로직
-        if (keyboard.cKey.wasPressedThisFrame)
+        // 5. 공격
+        if (keyboard.cKey.wasPressedThisFrame) // 키를 누른 그 순간에만 실행
         {
-            Shoot();
-            anim.SetTrigger("atShoot"); // [추가] 총쏘기 애니메이션 트리거
+            if (Time.time - lastShootTime > shootDelay)
+            {
+                lastShootTime = Time.time; // 현재 시간 기록
+
+                Shoot();
+                anim.ResetTrigger("atShoot");
+                anim.SetTrigger("atShoot");
+            }
         }
 
-        // ==========================================
-        // [추가] 애니메이션 파라미터 업데이트 섹션
-        // ==========================================
+        bool isActuallyMoving = moveInput != 0;
+        bool isCKeyHeld = keyboard.cKey.isPressed;
+
+        // 이동 중이거나, C키를 누르고 있는 동안에는 isMoving을 true로 고정
+        //anim.SetBool("isMoving", isActuallyMoving || isCKeyHeld);
+        //anim.SetBool("isGrounded", isGrounded);
+
+
         // 이동 중인지 여부 (속도의 절대값이 0보다 크면 걷는 중)
         anim.SetBool("isMoving", moveInput != 0);
 
